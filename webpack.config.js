438ffdbbbeb209d0cf77334cf44fe810ae8e9692
MAbdurahman <mdbdrrhm@gmail.com>
2023-07-09 07:08:56 -0500

    Add configuration for html-webpack-server, hot-load, and html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
   },
-  
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack-5 For Beginners',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
 };
